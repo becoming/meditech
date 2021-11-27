e1654a6305a07bf64eb90e53b2d98738c173be34
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-11-27 15:18:17 +0100

    updated the mappe

--- crm/src/main/java/tech/becoming/medical/crm/patient/PatientMapper.java
+++ crm/src/main/java/tech/becoming/medical/crm/patient/PatientMapper.java
@@ -11,21 +11,29 @@ import tech.becoming.medical.crm.patient.entity.PatientDetails;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 //https://www.baeldung.com/mapstruct#1-modify-the-mapper
+//https://www.baeldung.com/mapstruct-custom-mapper#custom-mapper-annotation
+//https://www.baeldung.com/mapstruct-ignore-unmapped-properties#ignore-specific-fields
+//https://stackoverflow.com/a/62548185/1107450
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
 
-    default List<PatientView> toDto(Page<Patient> page) {
-        return page
-                .map(this::toDto)
+    default List<PatientView> toDto(Page<Patient> v) {
+        return v.map(this::toDto)
                 .getContent();
     }
 
-//    https://www.baeldung.com/mapstruct-custom-mapper#custom-mapper-annotation
-    @Mapping(source = "publisher.id", target = "publisherId")
-    @Mapping(source = "publisher.name", target = "publisherName")
-    PatientView toDto(Patient gg);
+    PatientView toDto(Patient v);
+
+    default UUID toUUID(String v) {
+        return v != null ? UUID.fromString(v) : null;
+    }
+
+    default String toString(UUID v) {
+        return v != null ? v.toString() : null;
+    }
 
     default Set<PatientView> toDto(Iterable<Patient> games) {
         var result = new HashSet<PatientView>();
@@ -34,8 +42,5 @@ public interface PatientMapper {
         return result;
     }
 
-//    https://www.baeldung.com/mapstruct-ignore-unmapped-properties#ignore-specific-fields
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "publisher", ignore = true)
     PatientDetails toEntity(NewPatientDetails dto);
 }
--- crm/src/main/java/tech/becoming/medical/crm/patient/dto/BasicDto.java
+++ crm/src/main/java/tech/becoming/medical/crm/patient/dto/BasicDto.java
@@ -1,15 +1,21 @@
 package tech.becoming.medical.crm.patient.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
+// why instant https://stackoverflow.com/questions/32437550/whats-the-difference-between-instant-and-localdatetime
+// how to use it https://stackoverflow.com/a/45674593/1107450
 @Getter
 @Setter
 public abstract class BasicDto {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "UTC")
     private Instant created;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "UTC")
     private Instant updated;
 
 }
