bcde3a1a6564e4cc4672a3bbe28a37ae55958958
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-11-27 14:03:38 +0100

    initial commit with minimal dependencies

---
+++ crm/src/test/java/tech/becoming/medical/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.becoming.medical.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
