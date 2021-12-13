93c81dbc33701502da592a05fbbd0585e95e4c5c
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-12-13 19:52:04 +0100

    added logo

--- crm-front/src/components/menu.tsx
+++ crm-front/src/components/menu.tsx
@@ -1,10 +1,13 @@
 import {Alignment, Button, Classes, Navbar, NavbarDivider, NavbarGroup, NavbarHeading} from "@blueprintjs/core";
+import {ReactComponent as Logo} from "../Logo-Becoming-Tech.svg"
 
 export function Menu() {
   return <div className={"bp4-dark"}>
     <Navbar>
       <NavbarGroup align={Alignment.LEFT}>
-        <NavbarHeading>TME // CRM Front</NavbarHeading>
+        <NavbarHeading>
+          <Logo className={"App-logo"} />
+        </NavbarHeading>
         <NavbarDivider />
         <Button className={Classes.MINIMAL + " .bp4-dark"} icon="home" text="Home" />
         <Button className={Classes.MINIMAL} icon="document" text="Files" />
