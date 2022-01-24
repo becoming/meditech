72d10b7ce0cc15bc9a6e8f1870b99d869a735424
Rodislav MOLDOVAN <rodislav@gmail.com>
2022-01-24 17:56:55 +0100

    put back the save identity message

--- crm-front/src/app/patients/components/forms/IdentityEditForm.tsx
+++ crm-front/src/app/patients/components/forms/IdentityEditForm.tsx
@@ -72,6 +72,7 @@ export function IdentityEditForm(props: Props) {
       <FormActionButtons disabled={disabled}
                          cancelLink={`/patients/${props.patientId}`}
                          onSave={onSave}
+                         saveLabel={"Save identity"}
                          error={error} />
     </div>
 
