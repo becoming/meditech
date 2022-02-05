685386e5fd3c74907dca4f36cc2776df98f3473a
Rodislav MOLDOVAN <rodislav@gmail.com>
2022-02-05 14:51:13 +0100

    renamed variables

--- crm-back/src/main/java/tech/becoming/medical/crm/patient/PatientService.java
+++ crm-back/src/main/java/tech/becoming/medical/crm/patient/PatientService.java
@@ -25,38 +25,38 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class PatientService {
 
-    private final PatientHelper helper;
+    private final PatientHelper patientHelper;
     private final PatientRepository patientRepository;
     private final IdentityRepository identityRepository;
     private final AddressRepository addressRepository;
     private final AddressMapper addressMapper;
-    private final PatientMapper mapper;
+    private final PatientMapper patientMapper;
 
     public Try<PatientDTO> findById(UUID id) {
         return Try.of(() -> id)
                 .map(patientRepository::findById)
                 .map(NotFoundException::throwIfEmpty)
-                .map(mapper::toDto)
+                .map(patientMapper::toDto)
                 .onFailure(e -> log.error("Could not perform the find in range, e: {}", e.getMessage()));
     }
 
     public Try<List<PatientDTO>> findInRange(PageRequest p) {
         return Try.of(() -> p)
-                .map(helper::validate)
+                .map(patientHelper::validate)
                 .map(patientRepository::findAll)
-                .map(mapper::toDto)
+                .map(patientMapper::toDto)
                 .onFailure(e -> log.error("Could not perform the find in range, e: {}", e.getMessage()));
     }
 
     public Try<PatientDTO> create(NewIdentityDTO p) {
         return Try.of(() -> p)
-                .map(helper::validate)
-                .map(mapper::toEntity)
+                .map(patientHelper::validate)
+                .map(patientMapper::toEntity)
                 .map(PatientEntity::setupNew)
                 .map(patientRepository::save) // FIXME start, solve this double save
                 .map(this::saveIdentity)
                 .map(patientRepository::save) // FIXME end
-                .map(mapper::toDto)
+                .map(patientMapper::toDto)
                 .onFailure(e -> log.error("Could not create a new patient, e: {}", e.getMessage()));
     }
 
@@ -80,7 +80,7 @@ public class PatientService {
                 .map(BadRequestException::throwIfFalse)
                 .map($ -> identityRepository.findById(identityId))
                 .map(NotFoundException::throwIfEmpty)
-                .map(mapper::toDto);
+                .map(patientMapper::toDto);
     }
 
     private AddressEntity addAddressToPatient(UUID patientId, AddressEntity addressEntity) {
