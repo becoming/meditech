3122fa43ddddc32eb8c216bd7853277f5c201ea8
Rodislav MOLDOVAN <rodislav@gmail.com>
2022-06-18 16:56:23 +0200

    extracted health check in a dedicated function

--- frontend/src/app/about/AboutButton.tsx
+++ frontend/src/app/about/AboutButton.tsx
@@ -4,30 +4,34 @@ import {httpHelper} from "../common/helpers/http/HttpHelper";
 import {IconName} from "@blueprintjs/icons";
 import {Intent} from "@blueprintjs/core/lib/esm/common/intent";
 
+const url = `${process.env.REACT_APP_BACKEND_SCHEME}://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}${process.env.REACT_APP_BACKEND_HEALTH}`
+
 export function AboutButton() {
 
   let [icon, setIcon] = useState<IconName>("info-sign")
   let [intent, setIntent] = useState<Intent>("none")
   let [text, setText] = useState<string>("About")
 
-  useEffect(() => {
-    let url = `${process.env.REACT_APP_BACKEND_SCHEME}://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}${process.env.REACT_APP_BACKEND_HEALTH}`
+  let callHealth = () => {
+    httpHelper.get<String>(url)
+      .subscribe({
+        next: () => {
+          setIcon("info-sign")
+          setIntent("none")
+          setText("About")
+        },
+        error: () => {
+          setIcon("offline")
+          setIntent("warning")
+          setText("About (offline)")
+        }
+      })
+  }
 
-    const interval = setInterval(() => {
-      httpHelper.get<String>(url)
-        .subscribe({
-          next: () => {
-            setIcon("info-sign")
-            setIntent("none")
-            setText("About")
-          },
-          error: () => {
-            setIcon("offline")
-            setIntent("warning")
-            setText("About (offline)")
-          }
-        })
-    }, 10000);
+  useEffect(() => {
+    callHealth();
+
+    const interval = setInterval(callHealth, 10000);
     return () => clearInterval(interval);
   }, []);
 
